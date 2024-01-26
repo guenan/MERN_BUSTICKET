d97caf2e50deddf72315d44d89a4e43a3a9ae41a
lenanguelo <audrey.nangue@googlemail.com>
2024-01-26 21:21:06 +0100

    handle users and peemissions

--- client/src/App.js
+++ client/src/App.js
@@ -52,6 +52,7 @@ function App() {
                             </ProtectedRoute>
                         }
                     />
+
                     <Route
                         path="/admin/buses"
                         element={
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -45,6 +45,15 @@ router.post("/login", async (req, res) => {
             });
         }
 
+
+        if (userExists.isBlocked) {
+            return res.send({
+                message: "Your account is blocked , please contact admin",
+                success: false,
+                data: null,
+            });
+        }
+
         const passwordMatch = await bcrypt.compare(
             req.body.password,
             userExists.password
@@ -88,6 +97,43 @@ router.post("/get-user-by-id", authMiddleware, async (req, res) => {
             data: user,
         });
     } catch (error) {
+        res.send({
+            message: error.message,
+            success: false,
+            data: null,
+        });
+    }
+});
+
+// get all users
+router.post("/get-all-users", authMiddleware, async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send({
+            message: "Users fetched successfully",
+            success: true,
+            data: users,
+        });
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false,
+            data: null,
+        });
+    }
+});
+
+// update user
+
+router.post("/update-user-permissions", authMiddleware, async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(req.body._id, req.body);
+        res.send({
+            message: "User permissions updated successfully",
+            success: true,
+            data: null,
+        });
+    } catch {
         res.send({
             message: error.message,
             success: false,
